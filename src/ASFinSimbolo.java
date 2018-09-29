558c195fa7cefc76bca71c2c46c8993299166c11
Gregorio Nicolas Gerardi <gregorionicolasgerardi@gmail.com>
2018-09-29 17:28:04 -0300

    errores incorporados sin testear

--- src/ASFinSimbolo.java
+++ src/ASFinSimbolo.java
@@ -1,2 +1,8 @@
-public class ASFinSimbolo {
+public class ASFinSimbolo implements AccionSemantica{
+    @Override
+    public void ejecutar(AnalizadorLexico al) {
+        al.addListaDeTokens(String.format("SimboloSimple %2$s (linea %1$d)", al.getLinea(), al.getBuffer()));
+        String buffer = al.getBuffer();
+        al.setTokenActual(al.getIDforPR(buffer));
+    }
 }
